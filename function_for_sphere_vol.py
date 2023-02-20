fb83e529b2f0ffab01ed3202a3a95877199bf41e
Martin Njue <85220259+njue752@users.noreply.github.com>
2023-02-20 18:47:10 +0300

    Add files via upload

---
+++ function_for_sphere_vol.py
@@ -0,0 +1,7 @@
+#function to find the volume of a sphere
+def sphere_vol(Radius):
+    sphere_volume=4/3*3.142*Radius*Radius*Radius
+    print("The volume of the sphere is",sphere_volume)
+
+sphere_vol(7)
+
